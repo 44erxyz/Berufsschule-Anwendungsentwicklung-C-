9360f55018879e94eb5c22168dfdc76f15c05475
Maxi <moonxyz@outlook.de>
2026-01-13 08:34:14 +0100

    Update main.cpp

--- 13.11/main.cpp
+++ 13.11/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+int main() {
+    std::cout << "Kopfzeile Drehkreuzsteuerung";
+    for (int i = 30; i > 0; i--) {
+        std::cout << "Noch " << i << " Plätze frei" << std::endl;
+        std::cout << "Bitte einsteigen";
+        getchar();
+        std::cout << "Signal Drehkreuz";
+
+    }
+
+    std::cout << "Keine freien Plätze verfügbar" << std::endl;
+    std::cout << "Bitte nicht mehr einsteigen" << std::endl;
+
+}
